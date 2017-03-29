011454764fa0ef8b0e6a81a7dd37f311eeadd75f
Joe Kutner <jpkutner@gmail.com>
2017-03-29 16:58:37 -0500

    Updated release script

--- release.sh
+++ release.sh
@@ -3,6 +3,17 @@
 set -o pipefail
 set -eu
 
+VERSION="${1}"
+
+mvn versions:set -DnewVersion=
+
+rm pom.xml.versionsBackup
+
+git add pom.xml
+
+git commit -m "Set version $VERSION"
+
 mvn clean package
 
-aws s3 cp target/heroku-metrics-agent.jar s3://lang-jvm/heroku-metrics-agent.jar --acl public-read
+aws s3 cp target/heroku-java-metrics-agent-$VERSION.jar s3://lang-jvm/heroku-java-metrics-agent-$VERSION.jar --acl public-read
+aws s3 cp s3://lang-jvm/heroku-java-metrics-agent-$VERSION.jar s3://lang-jvm/heroku-metrics-agent.jar --acl public-read
