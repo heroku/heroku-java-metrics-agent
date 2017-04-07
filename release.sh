911c900e9dfc673b143ed6f03b984580f4f17d06
Joe Kutner <jpkutner@gmail.com>
2017-04-07 16:09:55 -0500

    Updated release script

--- release.sh
+++ release.sh
@@ -16,4 +16,12 @@ git commit -m "Set version $VERSION"
 mvn clean package
 
 aws s3 cp target/heroku-metrics-agent.jar s3://lang-jvm/heroku-metrics-agent-$VERSION.jar --acl public-read
-aws s3 cp s3://lang-jvm/heroku-metrics-agent-$VERSION.jar s3://lang-jvm/heroku-metrics-agent.jar --acl public-read
+
+echo -n "Promote this version? (y/N) "
+read -s -n 1 promote
+
+if [ "$promote" = "y" ]; then
+  aws s3 cp s3://lang-jvm/heroku-metrics-agent-$VERSION.jar s3://lang-jvm/heroku-metrics-agent.jar --acl public-read
+fi
+
+echo "Done."
