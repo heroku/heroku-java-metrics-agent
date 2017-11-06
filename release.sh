7054bf212e22c05cb16c37aeebf7023e697edaba
BuildpackTester <jpkutner@gmail.com>
2017-11-06 10:57:12 -0600

    udpated release script

--- release.sh
+++ release.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
-set -o pipefail
-set -eu
+set -e
 
-VERSION="${1}"
+# Releasing to Maven Central can be confusing. So this will partially automate
+# the process.
+# http://central.sonatype.org/pages/apache-maven.html
 
-mvn versions:set -DnewVersion=$VERSION
+# First, make sure you have your gpg key setup
+# http://central.sonatype.org/pages/working-with-pgp-signatures.html
 
-rm pom.xml.versionsBackup
+# Then make sure your ~/.m2/settings.xml has this stuff in it:
+#
+# <server>
+#   <id>ossrh</id>
+#     <username>youruser</username>
+#     <password>XXXXXXXXXX</password>
+#   </server>
+# </servers>
+#
+# <profiles>
+#   <profile>
+#     <id>ossrh</id>
+#     <activation>
+#     <activeByDefault>true</activeByDefault>
+#     </activation>
+#     <properties>
+#       <gpg.executable>gpg</gpg.executable>
+#       <gpg.passphrase>XXXXXXXXXX</gpg.passphrase>
+#     </properties>
+#   </profile>
+# </profiles>
 
-git add pom.xml
+# Now we can prepare for release (it's not really permantent, but there's no harm in overreacting at this point)
+read -p "Are you sure you want to release? It's permanent! (press any key to continue)"
+mvn release:clean release:prepare
 
-git commit -m "Set version $VERSION"
-
-mvn clean package
-
-aws s3 cp target/heroku-metrics-agent.jar s3://lang-jvm/heroku-metrics-agent-$VERSION.jar --acl public-read
-
-echo -n "Promote this version? (y/N) "
-read -s -n 1 promote
-
-if [ "$promote" = "y" ]; then
-  aws s3 cp s3://lang-jvm/heroku-metrics-agent-$VERSION.jar s3://lang-jvm/heroku-metrics-agent.jar --acl public-read
-fi
-
-echo "Done."
+# Now we can actually release (it's slightly more permantent now)
+read -p "Are you still sure you want to release? It's permanent! (press any key to continue)"
+mvn release:perform
