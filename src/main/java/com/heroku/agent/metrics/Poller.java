ad1c430f1846d364c84245b706bacd689867dc8a
Joe Kutner <jpkutner@gmail.com>
2018-01-09 17:11:46 -0600

    Report every 5 seconds, not every 5 milliseconds

--- src/main/java/com/heroku/agent/metrics/Poller.java
+++ src/main/java/com/heroku/agent/metrics/Poller.java
@@ -71,7 +71,7 @@ public class Poller {
         previousCounters = counters;
         callback.apply(mapper, metricsJson);
       }
-    }, 5, 5);
+    }, 5000, 5000);
   }
 
   public void cancel() {
